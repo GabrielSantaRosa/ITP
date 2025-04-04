0e5e671ce5bcf893cad3735466b2bf89d2333184
Gabriel Cortez <96384061+GabrielSantaRosa@users.noreply.github.com>
2025-04-03 21:45:02 -0300

    Add files via upload

---
+++ Programas/index.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    char x1;
+    char converso = 32;
+    char tratado;
+
+    cin >> x1;
+
+    tratado = x1 + converso;
+
+    cout << "A letra " << x1 << " Agora is " << tratado << endl;
+
+    return 0;
+}
