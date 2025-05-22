71c7b91c683b1c23220ab13622569abb9cfa406a
GabrielSantaRosa <gabrielsanta279@gmail.com>
2025-05-22 07:22:42 -0300

    Aprendendo a ultilizar String em C++

--- Main.cpp
+++ Main.cpp
@@ -1,38 +1,30 @@
 #include <iostream>
+#include <string>
+#include <cctype>
+
+///#include <cstring>
 
 using namespace std;
 
 int main()
 {
 
-    bool Repete = true;
-    int contador = 0;
-    int VitoriasInter = 0, VitoriasGremio = 0, Empates = 0;
-    while(Repete)
+    string texto;
+    getline(cin, texto);
+
+    texto[0] = toupper(texto[0]);
+
+    for(int i = 1; texto.length() > i; i++)
     {
-        int GolInter, GolGremio;
-        int SimOuNao;
-        contador +=1;
 
-        cin >> GolInter >> GolGremio;
-
-        if(GolInter > GolGremio) VitoriasInter+=1;
-        else if(GolInter < GolGremio) VitoriasGremio+=1;
-        else Empates+=1;
-
-        cout << "Novo grenal (1-sim 2-nao)" << endl;
-        cin >> SimOuNao;
-        if(SimOuNao == 2) Repete = false;
+        if(texto[i-1] == ' ')
+        {
+            texto[i] = toupper(texto[i]);
+        }
     }
 
-    cout << contador << " grenais" << endl;
-    cout << "Inter:" << VitoriasInter << endl;
-    cout << "Gremio:" << VitoriasGremio << endl;
-    cout << "Empates:" << Empates << endl;
 
-    if(VitoriasInter > VitoriasGremio) cout << "Inter venceu mais" << endl;
-    else if(VitoriasInter < VitoriasGremio) cout << "Inter venceu mais" << endl;
-    else cout << "Nao houve vencedor" << endl;
+    cout << texto << endl;
 
     return 0;
 }
