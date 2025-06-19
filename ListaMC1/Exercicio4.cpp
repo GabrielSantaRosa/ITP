c0842293745617921e5e03c67722d295d8d60f4a
GabrielSantaRosa <gabrielsanta279@gmail.com>
2025-06-19 18:47:33 -0300

    feito

--- ListaMC1/Exercicio4.cpp
+++ ListaMC1/Exercicio4.cpp
@@ -1,75 +1,153 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-struct Bizout
+//Topico 3, questão 1;
+long long Div(long long dividendo, long long divisor)
 {
-    int aBizo;
-    int bBizo;
-    int qBizo;
-    int xBizo;
-    int yBizo;
-};
-
-void Exibe(Bizout b[], int tam)
-{
-    for(int i = tam; i >= 0 ; i--)
+    long long quociente = 0;
+    if(dividendo > 0 )
     {
-        cout << b[i].aBizo << endl;
-        cout << b[i].bBizo << endl;
-        cout << b[i].qBizo << endl;
-        cout << b[i].xBizo << endl;
-        cout << b[i].yBizo << endl;
-        cout << "========" << endl;
+        if( divisor > 0){
+            while( divisor <= dividendo)
+            {
+                quociente++;
+                dividendo = dividendo - divisor;
+            } 
+        }
+        else if(divisor < 0)
+        {
+            divisor = -1 * divisor; 
+            while( divisor <= dividendo)
+            {
+                quociente++;
+                dividendo = dividendo - divisor;
+            }
+            quociente = -1 * quociente;
+        }
+        else
+        {
+            ///cout << "Inderfinido" << endl;
+            return 0;
+        }
     }
+    else if (dividendo < 0)
+    {
+        if(divisor > 0) //Maior que 0
+        {
+            while(dividendo < 0)
+            {
+                quociente--;
+                dividendo = dividendo + divisor;
+            }
+            return quociente;
+        }
+        else if(divisor < 0) //Menor que 0
+        {
+            while(dividendo < 0)
+            {
+                quociente++;
+                dividendo = dividendo - divisor;
+            }
+            return quociente;
+        }
+        // else
+        // {
+        //     cout << "Inderfinido" << endl;
+        // }
+    }
+    else
+    {
+        return dividendo;
+    }
+    return quociente;
 }
 
-void AlgoritmoEstendidoEuclides(int a, int b)
+//Topico 4, questão 1
+long long Mod(long long dividendo, long divisor)
 {
-    // a = abs(a);
-    // b = abs(b);
-    // if(b > a) 
-    // {
-    //     int temp = a;
-    //     a = b;
-    //     b = temp;
-    // }
-    //Vamos considerar que o X seja maior que o y;
-    int tamanhodoarraybizout = 1000; //Escolhi o a, pq não sei qual seria o tamanho exato do array
-    Bizout bizout[tamanhodoarraybizout];
-    int tamanhoatual = 0;
+    long long resto; 
+    resto = dividendo - divisor * Div(dividendo,divisor);
+    return resto;
+}
+
+struct Bizout
+{
+    long long aBizo;
+    long long bBizo;
+    long long qBizo;
+    long long xBizo;
+    long long yBizo;
+};
+// a * x + b * y = mdc(x,y);
+
+// void Exibe(Bizout b[], long longtam)
+// {
+//     for(long longi = tam; i >= 0 ; i--)
+//     {
+//         cout << b[i].aBizo << endl;
+//         cout << b[i].bBizo << endl;
+//         cout << b[i].qBizo << endl;
+//         cout << b[i].xBizo << endl;
+//         cout << b[i].yBizo << endl;
+//         cout << "========" << endl;
+//     }
+// }
+
+void AlgoritmoEstendidoEuclides(long long a, long long b)
+{
+    if(a == 0 && b == 0)
+    {
+        cout << "indefinido" << endl;
+        return;
+    }
+
+    long long a_guarda = a;
+    long long b_guarda = b;
+
+    vector<Bizout> VectorBizout;
+    Bizout bizout;
+
     while(b != 0)
     {
-        bizout[tamanhoatual].aBizo = a;
-        bizout[tamanhoatual].bBizo = b;
-        bizout[tamanhoatual].qBizo = a / b;
-        bizout[tamanhoatual].xBizo = 0;
-        bizout[tamanhoatual].yBizo = 0;
-        tamanhoatual++;
-        int temp = a % b;
+        bizout.aBizo = a;
+        bizout.bBizo = b;
+        bizout.qBizo = Div(a,b);
+        bizout.xBizo = 0;
+        bizout.yBizo = 0;
+        VectorBizout.push_back(bizout);
+        long long temp = Mod(a,b);
         a = b;
         b = temp;
     }
-    bizout[tamanhoatual].aBizo = a;
-    bizout[tamanhoatual].bBizo = b;
-    bizout[tamanhoatual].qBizo = 0; //Só para preencher pq seria a div de a | b onde b é zero;
-    bizout[tamanhoatual].xBizo = 1;
-    bizout[tamanhoatual].yBizo = 0;
 
-    cout << "Esse eh o MDC " << bizout[tamanhoatual].aBizo << endl;
+    //Caso quando o B seja em 0;
+    bizout.aBizo = a;
+    bizout.bBizo = b;
+    bizout.qBizo = 0;
+    bizout.xBizo = 1;
+    bizout.yBizo = 0;
+    VectorBizout.push_back(bizout);
 
-    for(int i = tamanhoatual; i > 0; i--)
+    cout << "Esse eh o MDC " << a << endl;
+
+    long long qtdElementos = VectorBizout.size() - 1; 
+    //Os endereços no vector comecam a contar aparti do 0;
+
+    for(long long i = qtdElementos; i > 0; i--)
     {
-        bizout[i-1].xBizo = bizout[i].yBizo;
-        bizout[i-1].yBizo = bizout[i].xBizo - bizout[i-1].qBizo * bizout[i].yBizo;
+        VectorBizout[i-1].xBizo = VectorBizout[i].yBizo;
+        VectorBizout[i-1].yBizo = VectorBizout[i].xBizo - VectorBizout[i-1].qBizo * VectorBizout[i].yBizo;
     }
-    cout << bizout[0].xBizo << " Valor de X" << endl;
-    cout << bizout[0].yBizo << " Valor de Y" << endl;
-}
+    
+    cout << "X = " << VectorBizout[0].xBizo << " , Y = " << VectorBizout[0].yBizo << endl;
 
+    cout << "(" <<a_guarda << " * " << VectorBizout[0].xBizo << ") + (" << b_guarda << " * " << VectorBizout[0].yBizo << ") = " << VectorBizout[qtdElementos].aBizo;
+}
 
 int main()
 {
-    AlgoritmoEstendidoEuclides(-24, 18);
+    AlgoritmoEstendidoEuclides(15648742654, 4);
     return 0;
 }
